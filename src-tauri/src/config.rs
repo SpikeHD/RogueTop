1ba5b889d63a825e28ca4ab2d5f835719f865a7b
SpikeHD <spikegdofficial@gmail.com>
2024-05-08 19:55:26 -0700

    fix: default online

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -35,7 +35,7 @@ pub fn write_config_file(contents: String) {
 #[tauri::command]
 pub fn default_config() -> Config {
   Config {
-    skip_splash: Some(false),
+    skip_splash: Some(true),
     offline: Some(false),
   }
 }
