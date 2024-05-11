27c73ffcbfc44c92bed3fa77ba553085ba364df1
SpikeHD <spikegdofficial@gmail.com>
2024-05-11 12:11:22 -0700

    fix: prevent keydown spam

--- ext/keys.ts
+++ ext/keys.ts
@@ -1,12 +1,10 @@
 // When the user presses F1, it should return to the main menu.
 export async function registerMenuKeybind() {
   document.addEventListener('keydown', async (e) => {
-    // @ts-expect-error womp womp
-    const isDev = await __TAURI_INTERNALS__.invoke('is_dev')
-
-    console.log(e.key)
-
     if (e.key === 'F1') {
+      // @ts-expect-error womp womp
+      const isDev = await __TAURI_INTERNALS__.invoke('is_dev')
+  
       window.location.href = isDev ? 'http://localhost:1420' : 'http://tauri.localhost'
     }
   })
