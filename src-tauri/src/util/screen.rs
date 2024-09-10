d1e1cc0d6d76d5a628146faef28ae55f51c9fedf
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2024-09-10 13:11:41 -0700

    fix: fullscreen on android

--- src-tauri/src/util/screen.rs
+++ src-tauri/src/util/screen.rs
@@ -1,6 +1,10 @@
 #[tauri::command]
 pub fn toggle_fullscreen(win: tauri::Window) {
+  #[cfg(not(any(target_os = "android", target_os = "ios")))]
   win
     .set_fullscreen(!win.is_fullscreen().unwrap_or_default())
     .unwrap_or_default();
+
+  #[cfg(any(target_os = "android", target_os = "ios"))]
+  return;
 }
